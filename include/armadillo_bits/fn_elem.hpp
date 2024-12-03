11e69548f2e28a270f8dc7b8eb0f8f9ddee8d68d
conrad <conrad@localhost.localdomain>
2024-12-03 16:25:38 +1000

    preserve vector type information

--- include/armadillo_bits/fn_elem.hpp
+++ include/armadillo_bits/fn_elem.hpp
@@ -764,12 +764,12 @@ conj(const SpBase<typename T1::pod_type,T1>& A)
 template<typename T1>
 arma_warn_unused
 arma_inline
-const eOp<T1, eop_conj>
-conj(const Base<std::complex<typename T1::pod_type>,T1>& A)
+typename enable_if2< (is_arma_type<T1>::value && is_cx<typename T1::elem_type>::yes), const eOp<T1, eop_conj> >::result
+conj(const T1& A)
   {
   arma_debug_sigprint();
   
-  return eOp<T1, eop_conj>(A.get_ref());
+  return eOp<T1, eop_conj>(A);
   }
 
 
