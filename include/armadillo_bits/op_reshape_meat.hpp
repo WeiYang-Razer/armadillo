81e7d78488e5d7354f8b1d52fb42dbca5ec81db2
conrad <conrad@localhost.localdomain>
2025-01-17 13:05:54 +1000

    don't fill with zeros as the memory will be immediately overwritten

--- include/armadillo_bits/op_reshape_meat.hpp
+++ include/armadillo_bits/op_reshape_meat.hpp
@@ -85,7 +85,7 @@ op_reshape::apply_mat_inplace(Mat<eT>& A, const uword new_n_rows, const uword ne
   
   if( (A.n_rows == new_n_cols) && (A.n_cols == new_n_rows) )  { A.set_size(new_n_rows, new_n_cols); return; }
   
-  Mat<eT> B(new_n_rows, new_n_cols);
+  Mat<eT> B(new_n_rows, new_n_cols, arma_nozeros_indicator());
   
   op_reshape::apply_mat_noalias(B, A, new_n_rows, new_n_cols);
   
@@ -211,7 +211,7 @@ op_reshape::apply_cube_inplace(Cube<eT>& A, const uword new_n_rows, const uword 
   
   if( (A.n_rows == new_n_cols) && (A.n_cols == new_n_rows) && (A.n_slices == new_n_slices) )  { A.set_size(new_n_rows, new_n_cols, new_n_slices); return; }
   
-  Cube<eT> B(new_n_rows, new_n_cols, new_n_slices);
+  Cube<eT> B(new_n_rows, new_n_cols, new_n_slices, arma_nozeros_indicator());
   
   op_reshape::apply_cube_noalias(B, A, new_n_rows, new_n_cols, new_n_slices);
   
