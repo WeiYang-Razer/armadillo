e7ecee425fcbae88e70561211bf3c65eb297447b
conrad <conrad@localhost.localdomain>
2025-02-03 14:55:59 +1000

    remove checking for matrix mul chains

--- include/armadillo_bits/glue_times_meat.hpp
+++ include/armadillo_bits/glue_times_meat.hpp
@@ -537,7 +537,7 @@ glue_times::apply_inplace_plus(Mat<typename T1::elem_type>& out, const Glue<T1, 
   typedef typename T1::elem_type            eT;
   typedef typename get_pod_type<eT>::result  T;
   
-  if( X.is_alias(out) || (is_glue_times<T1>::value) || (is_outer_product<T1>::value) || (has_op_inv_any<T1>::value) || (has_op_inv_any<T2>::value) )
+  if( X.is_alias(out) || (is_outer_product<T1>::value) || (has_op_inv_any<T1>::value) || (has_op_inv_any<T2>::value) )
     {
     // handle aliasing and partial workaround for corner cases
     
