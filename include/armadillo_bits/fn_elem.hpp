0a175fb070c72b5dd2372c0e34510c8547b0baca
conrad <conrad@localhost.localdomain>
2025-02-15 11:56:07 +1000

    comment out problematic workarounds

--- include/armadillo_bits/fn_elem.hpp
+++ include/armadillo_bits/fn_elem.hpp
@@ -767,17 +767,17 @@ pow(const T1& A, const typename T1::elem_type exponent)
 
 
 
-// workaround for old user code
-template<typename eT>
-arma_frown("use std::pow(arma::as_scalar(X),Y) instead")
-inline
-typename arma_scalar_only<eT>::result
-pow(const eT& A, const eT exponent)
-  {
-  arma_debug_sigprint();
-  
-  return eop_aux::pow(A, exponent);
-  }
+// // workaround for old user code
+// template<typename eT>
+// arma_frown("use std::pow(arma::as_scalar(X),Y) instead")
+// inline
+// typename arma_scalar_only<eT>::result
+// pow(const eT& A, const eT exponent)
+//   {
+//   arma_debug_sigprint();
+//   
+//   return eop_aux::pow(A, exponent);
+//   }
 
 
 
@@ -811,17 +811,17 @@ pow(const T1& A, const typename T1::elem_type::value_type exponent)
 
 
 
-// workaround for old user code
-template<typename eT>
-arma_frown("use std::pow(arma::as_scalar(X),Y) instead")
-inline
-typename enable_if2< is_cx<eT>::yes, eT >::result
-pow(const eT& A, const typename eT::value_type exponent)
-  {
-  arma_debug_sigprint();
-  
-  return eop_aux::pow(A, eT(exponent));
-  }
+// // workaround for old user code
+// template<typename eT>
+// arma_frown("use std::pow(arma::as_scalar(X),Y) instead")
+// inline
+// typename enable_if2< is_cx<eT>::yes, eT >::result
+// pow(const eT& A, const typename eT::value_type exponent)
+//   {
+//   arma_debug_sigprint();
+//   
+//   return eop_aux::pow(A, eT(exponent));
+//   }
 
 
 
