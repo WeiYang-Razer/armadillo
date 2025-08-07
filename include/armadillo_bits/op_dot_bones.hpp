dd1be88826be846988faed6a55929e67baebcea4
conrad <conrad@localhost.localdomain>
2025-08-07 16:35:02 +1000

    use forced optimisation for fp16 dot products

--- include/armadillo_bits/op_dot_bones.hpp
+++ include/armadillo_bits/op_dot_bones.hpp
@@ -32,6 +32,11 @@ struct op_dot
   
   template<typename eT>
   arma_hot inline static
+  typename arma_not_cx<eT>::result
+  direct_dot_generic_force_optimise(const uword n_elem, const eT* const A, const eT* const B);
+  
+  template<typename eT>
+  arma_hot inline static
   typename arma_cx_only<eT>::result
   direct_dot_generic(const uword n_elem, const eT* const A, const eT* const B);
   
