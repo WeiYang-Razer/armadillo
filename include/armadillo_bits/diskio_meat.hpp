0c54c275170bebc9f01f87638c38c4862a710c3b
conrad <conrad@localhost.localdomain>
2025-09-15 15:38:34 +1000

    reduce delta

--- include/armadillo_bits/diskio_meat.hpp
+++ include/armadillo_bits/diskio_meat.hpp
@@ -935,11 +935,11 @@ diskio::save_csv_ascii(const Mat<eT>& x, std::ostream& f, const char separator)
   uword x_n_rows = x.n_rows;
   uword x_n_cols = x.n_cols;
   
-  const eT eT_as_int_accuracy_upper_limit = (is_fp16<eT>::value) ? eT(0x800) : ( (is_float<eT>::value) ? eT(0x1000000) : eT(0x20000000000000) );
-  const eT eT_as_int_accuracy_lower_limit = -eT_as_int_accuracy_upper_limit;
+  const eT eT_int_accuracy_upper_limit = (is_fp16<eT>::value) ? eT(0x800) : ( (is_float<eT>::value) ? eT(0x1000000) : eT(0x20000000000000) );
+  const eT eT_int_accuracy_lower_limit = -eT_int_accuracy_upper_limit;
   
-  const eT eT_as_int_max = (std::min)( eT(std::numeric_limits<int>::max()),    eT_as_int_accuracy_upper_limit );
-  const eT eT_as_int_min = (std::max)( eT(std::numeric_limits<int>::lowest()), eT_as_int_accuracy_lower_limit );
+  const eT eT_int_lowest = (std::max)( eT(std::numeric_limits<int>::lowest()), eT_int_accuracy_lower_limit );
+  const eT eT_int_max    = (std::min)( eT(std::numeric_limits<int>::max()),    eT_int_accuracy_upper_limit );
   
   for(uword row=0; row < x_n_rows; ++row)
     {
@@ -947,7 +947,7 @@ diskio::save_csv_ascii(const Mat<eT>& x, std::ostream& f, const char separator)
       {
       const eT val = x.at(row,col);
       
-      const bool is_real_int = (is_real<eT>::yes) && arma_isfinite(val) && (val > eT_as_int_min) && (val < eT_as_int_max) && (eT(int(val)) == val);
+      const bool is_real_int = (is_real<eT>::yes) && arma_isfinite(val) && (val > eT_int_lowest) && (val < eT_int_max) && (eT(int(val)) == val);
       
       (is_real_int) ? arma_ostream::raw_print_elem(f, int(val)) : arma_ostream::raw_print_elem(f, val);
       
@@ -980,11 +980,11 @@ diskio::save_csv_ascii(const Mat< std::complex<T> >& x, std::ostream& f, const c
   
   diskio::prepare_stream<eT>(f);
   
-  const T T_as_int_accuracy_upper_limit = (is_fp16<T>::value) ? T(0x800) : ( (is_float<T>::value) ? T(0x1000000) : T(0x20000000000000) );
-  const T T_as_int_accuracy_lower_limit = -T_as_int_accuracy_upper_limit;
+  const T T_int_accuracy_upper_limit = (is_fp16<T>::value) ? T(0x800) : ( (is_float<T>::value) ? T(0x1000000) : T(0x20000000000000) );
+  const T T_int_accuracy_lower_limit = -T_int_accuracy_upper_limit;
   
-  const T T_as_int_max = (std::min)( T(std::numeric_limits<int>::max()),    T_as_int_accuracy_upper_limit );
-  const T T_as_int_min = (std::max)( T(std::numeric_limits<int>::lowest()), T_as_int_accuracy_lower_limit );
+  const T T_int_lowest = (std::max)( T(std::numeric_limits<int>::lowest()), T_int_accuracy_lower_limit );
+  const T T_int_max    = (std::min)( T(std::numeric_limits<int>::max()),    T_int_accuracy_upper_limit );
   
   uword x_n_rows = x.n_rows;
   uword x_n_cols = x.n_cols;
@@ -1000,8 +1000,8 @@ diskio::save_csv_ascii(const Mat< std::complex<T> >& x, std::ostream& f, const c
       const T    abs_i = (val_i < T(0)) ? T(-val_i) : T(val_i);
       const char sgn_i = (val_i < T(0)) ? char('-') : char('+');
       
-      const bool val_r_is_real_int = (is_real<T>::yes) && arma_isfinite(val_r) && (val_r > T_as_int_min) && (val_r < T_as_int_max) && (T(int(val_r)) == val_r);
-      const bool abs_i_is_real_int = (is_real<T>::yes) && arma_isfinite(abs_i)                           && (abs_i < T_as_int_max) && (T(int(abs_i)) == abs_i);
+      const bool val_r_is_real_int = (is_real<T>::yes) && arma_isfinite(val_r) && (val_r > T_int_lowest) && (val_r < T_int_max) && (T(int(val_r)) == val_r);
+      const bool abs_i_is_real_int = (is_real<T>::yes) && arma_isfinite(abs_i)                           && (abs_i < T_int_max) && (T(int(abs_i)) == abs_i);
       
       (val_r_is_real_int) ? arma_ostream::raw_print_elem(f, int(val_r)) : arma_ostream::raw_print_elem(f, val_r);
       
@@ -1069,11 +1069,11 @@ diskio::save_coord_ascii(const Mat<eT>& x, std::ostream& f)
   
   const eT eT_zero = eT(0);
   
-  const eT eT_as_int_accuracy_upper_limit = (is_fp16<eT>::value) ? eT(0x800) : ( (is_float<eT>::value) ? eT(0x1000000) : eT(0x20000000000000) );
-  const eT eT_as_int_accuracy_lower_limit = -eT_as_int_accuracy_upper_limit;
+  const eT eT_int_accuracy_upper_limit = (is_fp16<eT>::value) ? eT(0x800) : ( (is_float<eT>::value) ? eT(0x1000000) : eT(0x20000000000000) );
+  const eT eT_int_accuracy_lower_limit = -eT_int_accuracy_upper_limit;
   
-  const eT eT_as_int_max = (std::min)( eT(std::numeric_limits<int>::max()),    eT_as_int_accuracy_upper_limit );
-  const eT eT_as_int_min = (std::max)( eT(std::numeric_limits<int>::lowest()), eT_as_int_accuracy_lower_limit );
+  const eT eT_int_lowest = (std::max)( eT(std::numeric_limits<int>::lowest()), eT_int_accuracy_lower_limit );
+  const eT eT_int_max    = (std::min)( eT(std::numeric_limits<int>::max()),    eT_int_accuracy_upper_limit );
   
   for(uword col=0; col < x.n_cols; ++col)
   for(uword row=0; row < x.n_rows; ++row)
@@ -1085,7 +1085,7 @@ diskio::save_coord_ascii(const Mat<eT>& x, std::ostream& f)
     f << row;  f.put(' ');
     f << col;  f.put(' ');
     
-    const bool is_real_int = (is_real<eT>::yes) && arma_isfinite(val) && (val > eT_as_int_min) && (val < eT_as_int_max) && (eT(int(val)) == val);
+    const bool is_real_int = (is_real<eT>::yes) && arma_isfinite(val) && (val > eT_int_lowest) && (val < eT_int_max) && (eT(int(val)) == val);
     
     (is_real_int) ? arma_ostream::raw_print_elem(f, int(val)) : arma_ostream::raw_print_elem(f, val);
     
@@ -1128,11 +1128,11 @@ diskio::save_coord_ascii(const Mat< std::complex<T> >& x, std::ostream& f)
   
   const eT eT_zero = eT(0);
   
-  const T T_as_int_accuracy_upper_limit = (is_fp16<T>::value) ? T(0x800) : ( (is_float<T>::value) ? T(0x1000000) : T(0x20000000000000) );
-  const T T_as_int_accuracy_lower_limit = -T_as_int_accuracy_upper_limit;
+  const T T_int_accuracy_upper_limit = (is_fp16<T>::value) ? T(0x800) : ( (is_float<T>::value) ? T(0x1000000) : T(0x20000000000000) );
+  const T T_int_accuracy_lower_limit = -T_int_accuracy_upper_limit;
   
-  const T T_as_int_max = (std::min)( T(std::numeric_limits<int>::max()),    T_as_int_accuracy_upper_limit );
-  const T T_as_int_min = (std::max)( T(std::numeric_limits<int>::lowest()), T_as_int_accuracy_lower_limit );
+  const T T_int_lowest = (std::max)( T(std::numeric_limits<int>::lowest()), T_int_accuracy_lower_limit );
+  const T T_int_max    = (std::min)( T(std::numeric_limits<int>::max()),    T_int_accuracy_upper_limit );
   
   for(uword col=0; col < x.n_cols; ++col)
   for(uword row=0; row < x.n_rows; ++row)
@@ -1147,8 +1147,8 @@ diskio::save_coord_ascii(const Mat< std::complex<T> >& x, std::ostream& f)
     const T val_r = std::real(val);
     const T val_i = std::imag(val);
     
-    const bool val_r_is_real_int = (is_real<T>::yes) && arma_isfinite(val_r) && (val_r > T_as_int_min) && (val_r < T_as_int_max) && (T(int(val_r)) == val_r);
-    const bool val_i_is_real_int = (is_real<T>::yes) && arma_isfinite(val_i) && (val_i > T_as_int_min) && (val_i < T_as_int_max) && (T(int(val_i)) == val_i);
+    const bool val_r_is_real_int = (is_real<T>::yes) && arma_isfinite(val_r) && (val_r > T_int_lowest) && (val_r < T_int_max) && (T(int(val_r)) == val_r);
+    const bool val_i_is_real_int = (is_real<T>::yes) && arma_isfinite(val_i) && (val_i > T_int_lowest) && (val_i < T_int_max) && (T(int(val_i)) == val_i);
     
     (val_r_is_real_int) ? arma_ostream::raw_print_elem(f, int(val_r)) : arma_ostream::raw_print_elem(f, val_r);
     
@@ -2982,11 +2982,11 @@ diskio::save_csv_ascii(const SpMat<eT>& x, std::ostream& f, const char separator
   
   const eT eT_zero = eT(0);
   
-  const eT eT_as_int_accuracy_upper_limit = (is_fp16<eT>::value) ? eT(0x800) : ( (is_float<eT>::value) ? eT(0x1000000) : eT(0x20000000000000) );
-  const eT eT_as_int_accuracy_lower_limit = -eT_as_int_accuracy_upper_limit;
+  const eT eT_int_accuracy_upper_limit = (is_fp16<eT>::value) ? eT(0x800) : ( (is_float<eT>::value) ? eT(0x1000000) : eT(0x20000000000000) );
+  const eT eT_int_accuracy_lower_limit = -eT_int_accuracy_upper_limit;
   
-  const eT eT_as_int_max = (std::min)( eT(std::numeric_limits<int>::max()),    eT_as_int_accuracy_upper_limit );
-  const eT eT_as_int_min = (std::max)( eT(std::numeric_limits<int>::lowest()), eT_as_int_accuracy_lower_limit );
+  const eT eT_int_lowest = (std::max)( eT(std::numeric_limits<int>::lowest()), eT_int_accuracy_lower_limit );
+  const eT eT_int_max    = (std::min)( eT(std::numeric_limits<int>::max()),    eT_int_accuracy_upper_limit );
   
   for(uword row=0; row < x_n_rows; ++row)
     {
@@ -3000,7 +3000,7 @@ diskio::save_csv_ascii(const SpMat<eT>& x, std::ostream& f, const char separator
         }
       else
         {
-        const bool is_real_int = (is_real<eT>::yes) && arma_isfinite(val) && (val > eT_as_int_min) && (val < eT_as_int_max) && (eT(int(val)) == val);
+        const bool is_real_int = (is_real<eT>::yes) && arma_isfinite(val) && (val > eT_int_lowest) && (val < eT_int_max) && (eT(int(val)) == val);
         
         (is_real_int) ? arma_ostream::raw_print_elem(f, int(val)) : arma_ostream::raw_print_elem(f, val);
         }
@@ -3082,11 +3082,11 @@ diskio::save_coord_ascii(const SpMat<eT>& x, std::ostream& f)
   
   diskio::prepare_stream<eT>(f);
   
-  const eT eT_as_int_accuracy_upper_limit = (is_fp16<eT>::value) ? eT(0x800) : ( (is_float<eT>::value) ? eT(0x1000000) : eT(0x20000000000000) );
-  const eT eT_as_int_accuracy_lower_limit = -eT_as_int_accuracy_upper_limit;
+  const eT eT_int_accuracy_upper_limit = (is_fp16<eT>::value) ? eT(0x800) : ( (is_float<eT>::value) ? eT(0x1000000) : eT(0x20000000000000) );
+  const eT eT_int_accuracy_lower_limit = -eT_int_accuracy_upper_limit;
   
-  const eT eT_as_int_max = (std::min)( eT(std::numeric_limits<int>::max()),    eT_as_int_accuracy_upper_limit );
-  const eT eT_as_int_min = (std::max)( eT(std::numeric_limits<int>::lowest()), eT_as_int_accuracy_lower_limit );
+  const eT eT_int_lowest = (std::max)( eT(std::numeric_limits<int>::lowest()), eT_int_accuracy_lower_limit );
+  const eT eT_int_max    = (std::min)( eT(std::numeric_limits<int>::max()),    eT_int_accuracy_upper_limit );
   
   typename SpMat<eT>::const_iterator iter     = x.begin();
   typename SpMat<eT>::const_iterator iter_end = x.end();
@@ -3098,7 +3098,7 @@ diskio::save_coord_ascii(const SpMat<eT>& x, std::ostream& f)
     
     const eT val = (*iter);
     
-    const bool is_real_int = (is_real<eT>::yes) && arma_isfinite(val) && (val > eT_as_int_min) && (val < eT_as_int_max) && (eT(int(val)) == val);
+    const bool is_real_int = (is_real<eT>::yes) && arma_isfinite(val) && (val > eT_int_lowest) && (val < eT_int_max) && (eT(int(val)) == val);
     
     (is_real_int) ? arma_ostream::raw_print_elem(f, int(val)) : arma_ostream::raw_print_elem(f, val);
     
@@ -3141,11 +3141,11 @@ diskio::save_coord_ascii(const SpMat< std::complex<T> >& x, std::ostream& f)
   
   diskio::prepare_stream<eT>(f);
   
-  const T T_as_int_accuracy_upper_limit = (is_fp16<T>::value) ? T(0x800) : ( (is_float<T>::value) ? T(0x1000000) : T(0x20000000000000) );
-  const T T_as_int_accuracy_lower_limit = -T_as_int_accuracy_upper_limit;
+  const T T_int_accuracy_upper_limit = (is_fp16<T>::value) ? T(0x800) : ( (is_float<T>::value) ? T(0x1000000) : T(0x20000000000000) );
+  const T T_int_accuracy_lower_limit = -T_int_accuracy_upper_limit;
   
-  const T T_as_int_max = (std::min)( T(std::numeric_limits<int>::max()),    T_as_int_accuracy_upper_limit );
-  const T T_as_int_min = (std::max)( T(std::numeric_limits<int>::lowest()), T_as_int_accuracy_lower_limit );
+  const T T_int_lowest = (std::max)( T(std::numeric_limits<int>::lowest()), T_int_accuracy_lower_limit );
+  const T T_int_max    = (std::min)( T(std::numeric_limits<int>::max()),    T_int_accuracy_upper_limit );
   
   typename SpMat<eT>::const_iterator iter     = x.begin();
   typename SpMat<eT>::const_iterator iter_end = x.end();
@@ -3160,8 +3160,8 @@ diskio::save_coord_ascii(const SpMat< std::complex<T> >& x, std::ostream& f)
     const T val_r = std::real(val);
     const T val_i = std::imag(val);
     
-    const bool val_r_is_real_int = (is_real<T>::yes) && arma_isfinite(val_r) && (val_r > T_as_int_min) && (val_r < T_as_int_max) && (T(int(val_r)) == val_r);
-    const bool val_i_is_real_int = (is_real<T>::yes) && arma_isfinite(val_i) && (val_i > T_as_int_min) && (val_i < T_as_int_max) && (T(int(val_i)) == val_i);
+    const bool val_r_is_real_int = (is_real<T>::yes) && arma_isfinite(val_r) && (val_r > T_int_lowest) && (val_r < T_int_max) && (T(int(val_r)) == val_r);
+    const bool val_i_is_real_int = (is_real<T>::yes) && arma_isfinite(val_i) && (val_i > T_int_lowest) && (val_i < T_int_max) && (T(int(val_i)) == val_i);
     
     (val_r_is_real_int) ? arma_ostream::raw_print_elem(f, int(val_r)) : arma_ostream::raw_print_elem(f, val_r);
     
