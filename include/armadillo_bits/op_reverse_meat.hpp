0998f46768210155985c81404731923ddb62de54
conrad <conrad@localhost.localdomain>
2025-10-14 00:51:30 +1000

    restore previous detection of row vectors

--- include/armadillo_bits/op_reverse_meat.hpp
+++ include/armadillo_bits/op_reverse_meat.hpp
@@ -105,7 +105,7 @@ op_reverse_vec::apply(Mat<typename T1::elem_type>& out, const Op<T1,op_reverse_v
     
     if(&out == &(U.M))
       {
-      if( (T1::is_row) || U.M.is_rowvec() )
+      if((T1::is_xvec) ? bool(U.M.is_rowvec()) : bool(T1::is_row))
         {
         op_fliplr::apply_mat_inplace(out);
         }
@@ -126,7 +126,7 @@ op_reverse_vec::apply(Mat<typename T1::elem_type>& out, const Op<T1,op_reverse_v
     {
     Mat<eT> tmp;
     
-    if( (T1::is_row) || U.M.is_rowvec() )
+    if((T1::is_xvec) ? bool(U.M.is_rowvec()) : bool(T1::is_row))
       {
       op_fliplr::apply_mat_noalias(tmp, U.M);
       }
@@ -139,7 +139,7 @@ op_reverse_vec::apply(Mat<typename T1::elem_type>& out, const Op<T1,op_reverse_v
     }
   else
     {
-    if( (T1::is_row) || U.M.is_rowvec() )
+    if((T1::is_xvec) ? bool(U.M.is_rowvec()) : bool(T1::is_row))
       {
       op_fliplr::apply_mat_noalias(out, U.M);
       }
