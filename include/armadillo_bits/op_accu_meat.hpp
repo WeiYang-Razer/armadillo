5491a208336a04638b2c2313602eb67819b149e1
conrad <conrad@localhost.localdomain>
2026-03-13 12:16:51 +1000

    simplifications

--- include/armadillo_bits/op_accu_meat.hpp
+++ include/armadillo_bits/op_accu_meat.hpp
@@ -80,7 +80,7 @@ op_accu_mat::apply_proxy_at(const Proxy<T1>& P)
   const uword n_rows = P.get_n_rows();
   const uword n_cols = P.get_n_cols();
   
-  if( (n_rows == 0) || (n_cols == 0) )  { return eT(0); }
+  if(n_rows == 0)  { return eT(0); }
   
   eT val = eT(0);
   
@@ -148,7 +148,7 @@ op_accu_mat::apply_omit_helper(const Proxy<T1>& P, functor is_omitted)
     const uword n_rows = P.get_n_rows();
     const uword n_cols = P.get_n_cols();
     
-    if( (n_rows == 0) || (n_cols == 0) )  { return eT_zero; }
+    if(n_rows == 0)  { return eT_zero; }
     
     for(uword c=0; c < n_cols; ++c)
     for(uword r=0; r < n_rows; ++r)
@@ -756,7 +756,7 @@ op_accu_cube::apply_proxy_at(const ProxyCube<T1>& P)
   const uword n_cols   = P.get_n_cols();
   const uword n_slices = P.get_n_slices();
   
-  if( (n_rows == 0) || (n_cols == 0) || (n_slices == 0) )  { return eT(0); }
+  if( (n_rows == 0) || (n_cols == 0) )  { return eT(0); }
   
   eT val1 = eT(0);
   eT val2 = eT(0);
