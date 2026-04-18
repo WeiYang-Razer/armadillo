95cad27904a36c59efa4469646f382c159ac6da5
conrad <conrad@localhost.localdomain>
2026-04-18 14:13:49 +1000

    ensure default tolerance is finite

--- include/armadillo_bits/Base_meat.hpp
+++ include/armadillo_bits/Base_meat.hpp
@@ -927,6 +927,8 @@ Base_extra_yes<elem_type,derived>::is_sympd() const
   // default value for tol
   const T tol = T(100) * std::numeric_limits<T>::epsilon() * norm(X, "fro");
   
+  if(arma_isnonfinite(tol))  { return false; }
+  
   if(X.is_hermitian(tol) == false)  { return false; }
   
   if(X.is_empty())  { return false; }
